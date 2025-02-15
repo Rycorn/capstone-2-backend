a725681754ac97ffa601adcde7a0725c473ab534
Rycorn <44075236+Rycorn@users.noreply.github.com>
2025-02-14 23:08:05 -0800

    file add

---
+++ controllers/usersController.js
@@ -0,0 +1,68 @@
+const User = require('../model/User');
+
+const getAllUsers = async (req, res) => {
+    const Users = await User.find();
+    if (!Users) return res.status(204).json({ 'message': 'No Users found.' });
+    res.json(Users);
+}
+
+const createNewUser = async (req, res) => {
+    if (!req?.body?.firstname || !req?.body?.lastname) {
+        return res.status(400).json({ 'message': 'First and last names are required' });
+    }
+
+    try {
+        const result = await User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+
+        res.status(201).json(result);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const updateUser = async (req, res) => {
+    if (!req?.body?.id) {
+        return res.status(400).json({ 'message': 'ID parameter is required.' });
+    }
+
+    const User = await User.findOne({ _id: req.body.id }).exec();
+    if (!User) {
+        return res.status(204).json({ "message": `No User matches ID ${req.body.id}.` });
+    }
+    if (req.body?.wins) User.wins = req.body.wins;
+    if (req.body?.losses) User.lastname = req.body.losses;
+    const result = await User.save();
+    res.json(result);
+}
+
+const deleteUser = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({ 'message': 'User ID required.' });
+
+    const User = await User.findOne({ _id: req.body.id }).exec();
+    if (!User) {
+        return res.status(204).json({ "message": `No User matches ID ${req.body.id}.` });
+    }
+    const result = await User.deleteOne(); //{ _id: req.body.id }
+    res.json(result);
+}
+
+const getUser = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ 'message': 'User ID required.' });
+
+    const User = await User.findOne({ _id: req.params.id }).exec();
+    if (!User) {
+        return res.status(204).json({ "message": `No User matches ID ${req.params.id}.` });
+    }
+    res.json(User);
+}
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser,
+    getUser
+}
---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: [String],
+    wins: {
+        type: Number
+    },
+    losses: {
+        type: Number
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
